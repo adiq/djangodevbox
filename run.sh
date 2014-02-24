d4bee18804e6102c2ccc929d613281e5baef1eb4
Adrian Zmenda <adiq@adiq.eu>
2014-02-24 21:11:05 +0100

    Removed nginx restart handler. Fix uWSGI installation in virtualenv.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh vagrant@192.168.100.2 "cd /var/www/devsite && source ../devsite-env/bin/activate && pip install -r requirements.txt; ../devsite-env/bin/uwsgi --ini ../uwsgi.ini"
+ssh vagrant@192.168.100.2 "cd /var/www/devsite && source /var/www/devsite-env/bin/activate && pip install -r /var/www/devsite/requirements.txt; /var/www/devsite-env/bin/uwsgi --ini /var/www/uwsgi.ini"
--- ssh.sh
+++ ssh.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh vagrant@192.168.100.2 "cd /var/www/devsite && source ../devsite-env/bin/activate"
+ssh vagrant@192.168.100.2 "cd /var/www/devsite && source /var/www/devsite-env/bin/activate && /bin/bash"
