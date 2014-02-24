5c239277cc141423348a958c9ff2767732ccc12f
Adrian Zmenda <adiq@adiq.eu>
2014-02-24 21:46:53 +0100

    Remove ssh script, fix uwsgi config file, fix run script.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh vagrant@192.168.100.2 "cd /var/www/devsite && source /var/www/devsite-env/bin/activate && pip install -r /var/www/devsite/requirements.txt; /var/www/devsite-env/bin/uwsgi --ini /var/www/uwsgi.ini"
+ssh vagrant@192.168.100.2 "cd /var/www/devsite && source /var/www/devsite-env/bin/activate && sudo /var/www/devsite-env/bin/pip install -r /var/www/devsite/requirements.txt && sudo /var/www/devsite-env/bin/uwsgi --ini /var/www/uwsgi.ini"
--- ssh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ssh vagrant@192.168.100.2 "cd /var/www/devsite && source /var/www/devsite-env/bin/activate && /bin/bash"
