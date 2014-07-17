81a71c2ea89717b76a5c8b66663ab8eb873bc014
Adrian Zmenda <adiq@adiq.eu>
2014-07-18 00:48:07 +0200

    Update setup script.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+vagrant up --no-provision
 ssh vagrant@192.168.100.2 "sudo apt-get update -y && sudo apt-get install python -y"
-echo "Now you can use ssh.sh to open ssh connection or run.sh to run django project"
+echo "Now you can use 'vagrant ssh' to open ssh connection or ./run.sh to run django project."
 echo "Remember to configure project in provisioning/playbook.yml to succeed!"
