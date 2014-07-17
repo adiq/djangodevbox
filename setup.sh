ed48329bdd4e4279faa911a8a2db48f4c267cd7d
Adrian Zmenda <adiq@adiq.eu>
2014-07-18 01:26:52 +0200

    Setup script update. Add dep check, provisioning and more output.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+echo "DJANGODEVBOX > Booting up vagrant box..."
 vagrant up --no-provision
+echo "DJANGODEVBOX > Configuring vagrant box... (enter password 'vagrant' if needed)"
 ssh vagrant@192.168.100.2 "sudo apt-get update -y && sudo apt-get install python -y"
-echo "Now you can use 'vagrant ssh' to open ssh connection or ./run.sh to run django project."
-echo "Remember to configure project in provisioning/playbook.yml to succeed!"
+echo "DJANGODEVBOX > Now you can use 'vagrant ssh' to open ssh connection or ./run.sh to run django project."
+echo "DJANGODEVBOX > Remember to configure project in provisioning/playbook.yml to succeed!"
+
+read -p "DJANGODEVBOX > Did you setup playbook and copy your django project? (Y/n): " choice
+case "$choice" in 
+  y|Y ) hash ansible-playbook 2>/dev/null || { echo >&2 "DJANGODEVBOX > Looks like you dont have Ansible installed. Do it, now! Then run 'vagrant provision' manually."; exit 1; };
+        echo "DJANGODEVBOX > Thanks god. Running provisioning for you :)";
+        vagrant provision;;
+  * ) echo "DJANGODEVBOX > Thats not good. Configure playbook and copy your django project. Then run 'vagrant provision' manually :)";;
+esac
