8a6f4c0b7341b772a89365e637ed26afeadb2b0e
Adrian Zmenda <adiq@adiq.eu>
2014-07-18 03:55:32 +0200

    Run script update.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ssh vagrant@192.168.100.2 "cd /var/www/devsite && source /var/www/devsite-env/bin/activate && sudo /var/www/devsite-env/bin/pip install -r /var/www/devsite/requirements.txt && sudo /var/www/devsite-env/bin/uwsgi --ini /var/www/uwsgi.ini"
+vagrant ssh -c "sudo /var/www/devsite-env/bin/pip install -r /var/www/devsite/requirements.txt && sudo /var/www/devsite-env/bin/uwsgi --ini /var/www/uwsgi.ini"
